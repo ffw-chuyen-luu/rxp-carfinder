90d7926785fa1f67826950223e9ee23ad1959458
Chuyen Luu <chuyen.luu@ffw.com>
2024-02-26 13:54:07 +0700

    feat(api): create brand, category and car types.

---
+++ lib/types.ts
@@ -0,0 +1,33 @@
+import { Oil, Transmission } from "@prisma/client";
+
+export type Brand = {
+  id: string;
+  title: string;
+  logo: string;
+};
+
+export type Category = {
+  id: string;
+  name: string;
+};
+
+export type Car = {
+  id: string;
+  createdAt: Date;
+  slug: string;
+  title: string;
+  image: string;
+  description: string;
+  oil: Oil;
+  transmission: Transmission;
+  capacity: number;
+  fuelTank: number;
+  pricePerDay: number;
+  discountedPrice: number;
+  officialWebsite: string;
+  isFeatured: boolean;
+  brand: Brand;
+  brandId: string;
+  category: Category;
+  categoryId: string;
+};
