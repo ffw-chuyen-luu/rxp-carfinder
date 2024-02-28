6a1d50a79f28371390b8042aa9bdeb9827700fa8
Chuyen Luu <chuyen.luu@ffw.com>
2024-02-28 12:03:40 +0700

    feat: add menu with profile, login, signup and logout items

--- components/sitewide/Header.tsx
+++ components/sitewide/Header.tsx
@@ -1,27 +1,62 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import SearchSuggestions from "../ui/SearchSuggestions";
+import SearchSuggestions from "@/components/ui/SearchSuggestions";
+import { auth, signOut } from "@/lib/user/auth";
 
-const Header = () => {
+const Header = async () => {
+  const session = await auth();
+  const user = session?.user;
+
   return (
     <header className="bg-white w-full">
       <div className="container m-auto px-4">
-        <div className="py-4 text-center md:flex md:flex-row md:justify-between">
-          <div className="md:flex md:items-center md:gap-4">
+        <div className="py-4 text-center lg:flex lg:flex-row lg:justify-between">
+          <div className="lg:flex lg:items-center lg:gap-4">
             <Link href="/" className="mr-0 md:mr-36">
               <Image
                 src="/images/logo.svg"
                 alt="Car finder"
                 width="145"
                 height="26"
-                className="mx-auto md:mx-0"
+                className="mx-auto lg:mx-0"
               />
             </Link>
-            <div className="mt-6 md:mt-0">
+            <div className="mt-6 lg:mt-0">
               <SearchSuggestions />
             </div>
           </div>
+
+          <ul className="mt-2 lg:mt-0 font-bold flex items-center justify-end gap-2">
+            {user && (
+              <>
+                <li>
+                  <Link href={"/profile"}>Profile</Link>
+                </li>
+                <li>
+                  <form
+                    action={async () => {
+                      "use server";
+                      await signOut();
+                    }}
+                  >
+                    <button>Sign Out</button>
+                  </form>
+                </li>
+              </>
+            )}
+
+            {!user && (
+              <>
+                <li>
+                  <Link href={"/login"}>Login</Link>
+                </li>
+                <li>
+                  <Link href={"/signup"}>Register</Link>
+                </li>
+              </>
+            )}
+          </ul>
         </div>
       </div>
     </header>
